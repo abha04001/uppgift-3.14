ee133bab780f2b0938b4499cc5c55803ee0299f2
caspian <casa04001@utb.vaxjo.se>
2022-09-30 14:14:10 +0200

    Add project files.

---
+++ uppgift 3.14/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Uppgift_3._14
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiet?");
+            string svar = Console.ReadLine().ToLower();
+            if (svar == "j")
+            {
+                Console.WriteLine("Du är anställd");
+            }
+            else if (svar =="n")
+            {
+                Console.WriteLine("Du är inte anställd");
+            }
+            else
+            {
+                Console.WriteLine("Du är anställd om du har gått ut från gymnasiet och är under 22år");
+            }
+            
+        }
+    }
+}
